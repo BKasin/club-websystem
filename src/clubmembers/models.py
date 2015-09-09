4c5195a6a1848b7fbf29c5e380ff83ff4224c571
securedirective <kenpilot@gmail.com>
2015-09-09 10:07:50 -0700

    Removed old files from the trydjango sample; added custom bootstrap theme; updated templates

--- src/clubmembers/models.py
+++ src/clubmembers/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+# Create your models here.
+
 class Club(models.Model):
   id                    = models.AutoField(primary_key=True)
 
