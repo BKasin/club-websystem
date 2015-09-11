faa8a6ff90f0288098195cc2ed619b99123a7959
securedirective <kenpilot@gmail.com>
2015-09-10 23:20:19 -0700

    Tonight's changes

--- src/clubmembers/models.py
+++ src/clubmembers/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 from versatileimagefield.fields import VersatileImageField, PPOIField
 
 # Create your models here.
@@ -16,30 +17,27 @@ class Club(models.Model):
 
 class Member(models.Model):
   # Extends the built-in User model
-  user                  = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True)
+  user                  = models.OneToOneField(User, primary_key=True)
 
   # Contact info
-  phone                 = models.CharField(max_length=10, null=True)
+  phone                 = models.CharField(max_length=10, blank=True)
   texting_ok            = models.BooleanField()
 
   # Photo of user
   photo                = VersatileImageField(upload_to="memberphotos", width_field="photowidth", height_field="photoheight", ppoi_field="photoppoi", blank=True, default="")
   photowidth           = models.PositiveIntegerField(null=True, editable=False)
   photoheight          = models.PositiveIntegerField(null=True, editable=False)
-  photoppoi            = PPOIField(null=True, editable=False)
+  photoppoi            = PPOIField(blank=True, editable=False)
 
   # PIN number, used for signing in and out for meetings without requiring the user's full password
-  pin_hash              = models.CharField(max_length=120, null=True)
+  pin_hash              = models.CharField(max_length=120, blank=True)
 
   # Other information we may be interested in
-  shirt_size            = models.CharField(max_length=2, null=True)
-  acad_major            = models.CharField(max_length=50, null=True)
-  acad_minor            = models.CharField(max_length=50, null=True)
-  acad_concentration    = models.CharField(max_length=50, null=True)
-  acad_grad_qtr         = models.CharField(max_length=20, null=True)  # Format as "<qtr> <year>" without abbreviations. ex: "Spring 2015"
-
-  def getfullname(self):
-    return "%s %s"%(self.user.first_name, self.user.last_name)
+  shirt_size            = models.CharField(max_length=2, blank=True)
+  acad_major            = models.CharField(max_length=50, blank=True)
+  acad_minor            = models.CharField(max_length=50, blank=True)
+  acad_concentration    = models.CharField(max_length=50, blank=True)
+  acad_grad_qtr         = models.CharField(max_length=20, blank=True)  # Format as "<qtr> <year>" without abbreviations. ex: "Spring 2015"
 
   def __unicode__(self): #Python 3.3 is __str__
     return "%s %s <%s>"%(self.user.first_name, self.user.last_name, self.user.email)
@@ -66,4 +64,4 @@ class Membership(models.Model):
   shirt_received_date   = models.DateField(null=True)
 
   def __unicode__(self): #Python 3.3 is __str__
-    return "%s --> %s"%(self.member.getfullname, self.club.name_short)
+    return "%s --> %s"%(self.member.user.get_full_name, self.club.name_short)
