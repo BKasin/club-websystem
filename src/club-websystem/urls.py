3911d28f00bbc9325a7bcf92783a3d763d92f4c6
securedirective <kenpilot@gmail.com>
2015-09-19 22:52:38 -0700

    Making code indenting consistent

--- src/club-websystem/urls.py
+++ src/club-websystem/urls.py
@@ -1,3 +1,4 @@
+import os
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
@@ -5,20 +6,20 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    # Our site
-    url(r'^$', 'mainsite.views.home', name='home'),
-    url(r'^about/$', 'mainsite.views.about', name='about'),
-    url(r'^pagemd/(?P<page>\w+)/$', 'contentblocks.views.pagemd', name='pagemd'),
-    url(r'^calendar/', 'events.views.calendar', name='calendar'),
-    url(r'^eventjson/', 'events.views.eventjson', name='eventjson'),
-    url(r'^eventmodify/', 'events.views.eventmodify', name='eventmodify'),
-    url(r'^userprofile/', 'clubmembers.views.userprofile', name='userprofile'),
+  # Our site
+  url(r'^$', 'mainsite.views.home', name='home'),
+  url(r'^about/$', 'mainsite.views.about', name='about'),
+  url(r'^pagemd/(?P<page>\w+)/$', 'contentblocks.views.pagemd', name='pagemd'),
+  url(r'^calendar/', 'events.views.calendar', name='calendar'),
+  url(r'^eventjson/', 'events.views.eventjson', name='eventjson'),
+  url(r'^eventmodify/', 'events.views.eventmodify', name='eventmodify'),
+  url(r'^userprofile/', 'clubmembers.views.userprofile', name='userprofile'),
 
-    # Built-in pages
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('registration.backends.default.urls')),
+  # Built-in pages
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^accounts/', include('registration.backends.default.urls')),
 ]
 
 if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
