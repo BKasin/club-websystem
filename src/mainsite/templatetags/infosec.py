0acd723c7a24fe022bf58b3e9069a29cae56d675
securedirective <kenpilot@gmail.com>
2015-09-25 10:16:59 -0700

    Made the navbar dynamically generated

---
+++ src/mainsite/templatetags/infosec.py
@@ -0,0 +1,51 @@
+import fnmatch
+
+from django import template
+register = template.Library()
+
+navdata = (
+  ("Home", "/"),
+  ("Calendar", "/calendar/"),
+  ("Projects", (
+    ("Pen Test Network", "/project/pentest/"),
+    ("Raspberry Pi security system", "/project/rpi/"),
+    ("-"),
+    ("Start a project", "/startaproject/"),
+  ), "/pagemd/project_*"),
+  ("Competitions", (
+    ("CCDC", "/pagemd/competition_ccdc/"),
+    ("NCL", "/pagemd/competition_ncl/"),
+    ("ITC", "/pagemd/competition_itc/"),
+    ("Netwars", "/pagemd/competition_nw/"),
+    ("Cyber Patriot", "/pagemd/competition_cp/"),
+  ), "/pagemd/competition_*"),
+  ("Wiki", "#"),
+  ("Blog", "#"),
+  ("About", "/about/"),
+)
+
+def generatemenu(nd, curpage):
+  output = ""
+  for pgref in nd:
+    pagename = pgref[0]
+    if pagename=="-":
+      output += "<li class='divider'></li>"
+    else:
+      url = pgref[1]
+      if type(url)==type(()):
+        output += "<li class='%s dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>%s <span class='caret'></span></a><ul class='dropdown-menu' role='menu'>%s</ul></li>" % (
+          'active' if fnmatch.fnmatch(curpage, pgref[2]) else '',
+          pagename,
+          generatemenu(url, curpage)
+        )
+      else:
+        output += "<li class='%s'><a href='%s'>%s</a></li>" % (
+          'active' if url==curpage else '',
+          url,
+          pagename
+        )
+  return output
+
+@register.simple_tag(takes_context=True)
+def generatenav(context):
+  return generatemenu(navdata, context.request.path_info)
--- src/mainsite/views.py
+++ src/mainsite/views.py
@@ -9,44 +9,40 @@ from nav import infosec
 
 # Create your views here.
 def home(request):
-    nav = infosec.nav("home")
-    context = {
-        "nav": nav,
-    }
-    return render(request, "home.html", context)
+  return render(request, "home.html")
 
 def about(request):
-	title = 'Contact Us'
-	title_align_center = True
-	form = ContactForm(request.POST or None)
-	if form.is_valid():
-		# for key, value in form.cleaned_data.iteritems():
-		# 	print key, value
-		# 	#print form.cleaned_data.get(key)
-		form_email = form.cleaned_data.get("email")
-		form_message = form.cleaned_data.get("message")
-		form_full_name = form.cleaned_data.get("full_name")
-		# print email, message, full_name
-		subject = 'Site contact form'
-		from_email = settings.EMAIL_HOST_USER
-		to_email = [from_email, 'youotheremail@email.com']
-		contact_message = "%s: %s via %s"%(
-				form_full_name,
-				form_message,
-				form_email)
-		some_html_message = """
-		<h1>hello</h1>
-		"""
-		send_mail(subject,
-				contact_message,
-				from_email,
-				to_email,
-				html_message=some_html_message,
-				fail_silently=True)
+  title = 'Contact Us'
+  title_align_center = True
+  form = ContactForm(request.POST or None)
+  if form.is_valid():
+    # for key, value in form.cleaned_data.iteritems():
+    #   print key, value
+    #   #print form.cleaned_data.get(key)
+    form_email = form.cleaned_data.get("email")
+    form_message = form.cleaned_data.get("message")
+    form_full_name = form.cleaned_data.get("full_name")
+    # print email, message, full_name
+    subject = 'Site contact form'
+    from_email = settings.EMAIL_HOST_USER
+    to_email = [from_email, 'youotheremail@email.com']
+    contact_message = "%s: %s via %s"%(
+        form_full_name,
+        form_message,
+        form_email)
+    some_html_message = """
+    <h1>hello</h1>
+    """
+    send_mail(subject,
+        contact_message,
+        from_email,
+        to_email,
+        html_message=some_html_message,
+        fail_silently=True)
 
-	context = {
-		"form": form,
-		"title": title,
-		"title_align_center": title_align_center,
-	}
-	return render(request, "about.html", context)
+  context = {
+    "form": form,
+    "title": title,
+    "title_align_center": title_align_center,
+  }
+  return render(request, "about.html", context)
