BKasin/club-websystem

commit b074d033f5edc38d74e134a448db7c89fecd0943

Added contrib sites to installed apps because it is called by a file

src/clubwebsystem/settings/s_10_base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
   'django.contrib.contenttypes',
   'django.contrib.sessions',
   'django.contrib.messages',
+  'django.contrib.sites.models.Site',
   'django.contrib.staticfiles',
   # third party apps
   'crispy_forms',

commit 5d993e5046019c2d7ae0d24d605a142cb511a182

Fixed error caused by commit b074d033f5

src/clubwebsystem/settings/s_10_base.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
   'django.contrib.contenttypes',
   'django.contrib.sessions',
   'django.contrib.messages',
-  'django.contrib.sites.models.Site',
+  'django.contrib.sites',
   'django.contrib.staticfiles',
   # third party apps
   'crispy_forms',