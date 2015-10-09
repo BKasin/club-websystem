4b8532d58b4f176cadbcac8440a6943e2b94c904
securedirective <kenpilot@gmail.com>
2015-10-08 22:53:05 -0700

    Merged migrations from the 'emailpendingwidget' and 'master' branches

---
+++ src/clubmembers/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('clubmembers', '0005_auto_20151006_2234'),
+        ('clubmembers', '0005_auto_20151009_0022'),
+    ]
+
+    operations = [
+    ]
