58158c1af1cf5a0afa2d73676c75ccab319d0c8b
securedirective <kenpilot@gmail.com>
2015-12-23 12:35:57 -0800

    Moved some templates and static content to new directories

--- src/system/settings/base.py
+++ src/system/settings/base.py
@@ -71,7 +71,7 @@ MIDDLEWARE_CLASSES = [
 TEMPLATES = [
   {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [os.path.join(BASE_DIR, "templates")],
+    'DIRS': [os.path.join(BASE_DIR, "templates", "global")],
     'APP_DIRS': True,
     'OPTIONS': {
       'context_processors': [
